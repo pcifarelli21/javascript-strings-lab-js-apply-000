6e010bebcd83089146c262dc22cfed126162cd3a
ambitious-cyborg-1541 <pcifarelli21@gmail.com>
2019-10-22 17:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 
-var conversation = 'Let\'s talk about ${topic}.';
+var conversation = 'Let\'s talk about';
+
+consol.log(conversation.concat(topic))
